a7493a97cf9adcca616897ee9c03c585840fed5c
Devayush Bajaj <81802725+devayush-bajaj@users.noreply.github.com>
2022-07-22 11:24:09 +0100

    Update GreatestNumber.java

--- JAVA/Core JAVA/week-1/DAY-4/GreatestNumber.java
+++ JAVA/Core JAVA/week-1/DAY-4/GreatestNumber.java
@@ -4,7 +4,7 @@ public class GreatestNumber {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Input 1 : ");
         int userInput_1 = sc.nextInt();
-        System.out.println("Enter Input 1 : ");
+        System.out.println("Enter Input 2 : ");
         int userInput_2 = sc.nextInt();
         if(userInput_1 > userInput_2){
             System.out.println("Input 1 is greater : " + userInput_1);
@@ -22,7 +22,7 @@ public class GreatestNumber {
 /*      OUTPUT
 Enter Input 1 :
 12
-Enter Input 1 :
+Enter Input 2 :
 43
 Input 2 is greater : 12
 
